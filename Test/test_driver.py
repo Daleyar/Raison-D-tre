1a148987b5b60fe0080a073a49db5a4a7c268a43
Daleyar <ali.daley72@gmail.com>
2022-05-25 19:17:37 -0400

    test

--- Test/test_driver.py
+++ Test/test_driver.py
@@ -5,5 +5,4 @@ def test_title():
     driver = webdriver.Chrome()
     driver.get("https://raison-detre.herokuapp.com")
     assert driver.title == "Raison D'être", "should be 'Raison D'être'"
-    driver.quit()
- 
+    driver.close()
